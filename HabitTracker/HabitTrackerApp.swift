d6bcc228068da15410dd4bd6bac70b0c4c21cc7e
emiliaforsheim <emiliaforsheim@outlook.com>
2025-04-29 06:44:52 +0200

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Emilia Forsheim on 2025-04-29.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
